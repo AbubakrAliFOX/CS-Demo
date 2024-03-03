356f72a2399d584723b3eb15eb7607863a8c704c
Abubakr <thefox5000@gmail.com>
2024-03-03 15:19:14 +0300

    First c# console write

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("My first c# app!!");
+            Console.WriteLine();
+        }
+    }
+}
