7d02a27115bf2e5d8d8d11e817f529e8c789a8fe
Abubakr <thefox5000@gmail.com>
2024-03-08 16:38:02 +0300

    Try Parse date

--- Program.cs
+++ Program.cs
@@ -7,24 +7,10 @@ namespace Main
 
         static void Main(string[] args)
         {
-
-            DateTime dt = new DateTime(2023, 2, 21);
-
-            // Hours, Minutes, Seconds
-            TimeSpan ts = new TimeSpan(49, 25, 34);
-            Console.WriteLine(ts);
-            Console.WriteLine(ts.Days);
-            Console.WriteLine(ts.Hours);
-            Console.WriteLine(ts.Minutes);
-            Console.WriteLine(ts.Seconds);
-
-            //this will add time span to the date.
-            DateTime newDate = dt.Add(ts);
-
-            Console.WriteLine(newDate);
-
-
-            Console.ReadKey();
+            DateTime date1;
+            string dateString = "3/8/2024";
+            DateTime.TryParse(dateString, out date1);
+            Console.WriteLine(date1);
 
         }
     }
