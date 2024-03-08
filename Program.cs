cccbafb923f4198b5ef6b2ac64039610f9ff7139
Abubakr <thefox5000@gmail.com>
2024-03-08 14:57:27 +0300

    Implicitly Typed Variables

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My first c# app!! My name is {0} and I am from {1} \a","Abubakr", "Sudan");
+            var Name = "Abubakr";
+            Console.WriteLine("My first c# app!! My name is {0} and I am from {1}",Name, "Sudan");
         }
     }
 }
